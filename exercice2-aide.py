8500ff15b71d3370b2f3a42b421f5f7f5efc8c7b
formationPythonPC-Juin <laurent.astier@ac-limoges.fr>
2019-03-30 10:54:53 +0100

    ajout exercice2-aide

---
+++ exercice2-aide.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt # À COMPLÉTER : 
+from math import pi # À COMPLÉTER : 
+import numpy as np # À COMPLÉTER : 
+
+####################################
+# question 2 : variables de l'énoncé
+####################################
+
+# À COMPLÉTER
+
+
+#############################################
+## question 3 : construction du tableau des x
+#############################################
+debut = 0 # début de l'ensemble des x
+fin = 5 # fin de l'ensemble des x
+pas  = 0.01
+
+# À COMPLÉTER : décommentez la ligne ci-dessous et complétez la fonction pour construire le tableau X
+# X = np.arange()
+
+###############################################
+# question 4 : construction du tableau des I(x)
+###############################################
+# on construit le tableau I image de x par la fonction qui nous intéresse
+
+# À COMPLÉTER
+
+
+
+########################################
+# question 5 : construction de la courbe
+########################################
+
+# À COMPLÉTER : décommentez la ligne ci-dessous et complétez-la
+# plt.plot(,,'b-', label="")
+
+
+#####################################
+# question 6 : noms aux axes et titre
+#####################################
+plt.xlabel("axe d'espace")# nom de l'axe selon Ox
+
+# À COMPLÉTER : nom de l'axe selon Oy
+
+
+# À COMPLÉTER : mettre un titre
+
+
+###############################
+# question 7 : limites des axes
+###############################
+
+# À COMPLÉTER : faire que le graphe ait des valeurs limites selon Ox
+# À COMPLÉTER : faire que le graphe ait des valeurs limites selon Oy
+plt.grid()# on affiche une grille
+
+
+###################################
+# question 8 : légende et affichage
+###################################
+
+# À COMPLÉTER
+# 1 - construire une légende
+# 2 - afficher le graphe 
