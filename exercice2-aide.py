9d081491b8ee6d182dd47d21343e1c873635b99c
formationPythonPC-Juin <laurent.astier@ac-limoges.fr>
2019-04-01 15:21:44 +0200

    ajout exercice3-aide

--- exercice2-aide.py
+++ exercice2-aide.py
@@ -1,43 +1,37 @@
-import matplotlib.pyplot as plt # À COMPLÉTER : 
-from math import pi # À COMPLÉTER : 
-import numpy as np # À COMPLÉTER : 
+import matplotlib.pyplot as plt
 
-####################################
-# question 2 : variables de l'énoncé
-####################################
+######################################
+# coordonnées spatiales et temporelles
+######################################
 
-# À COMPLÉTER
+T = [0.0, 0.04, 0.08, 0.12, 0.16, 0.2, 0.24, 0.28, 0.32, 0.36, 0.4, 0.44, 0.48, 0.52, 0.56, 0.6, 0.64, 0.68, 0.72, 0.76, 0.8, 0.84, 0.88, 0.92, 0.96, 1.0, 1.04]
+
+X = [-0.953328037081172, -0.879995111151852, -0.799995555592592, -0.716662685218364, -0.636663129659105, -0.559996888914815, -0.479997333355555, -0.393331148166358, -0.313331592607099, -0.233332037047839, -0.149999166673611, -0.066666296299383, 0.013333259259877, 0.096666129634105, 0.179999000008333, 0.259998555567592, 0.343331425941821, 0.426664296316049, 0.506663851875308, 0.586663407434568, 0.663329648178858, 0.743329203738117, 0.819995444482407, 0.893328370411728, 0.973327925970987, 1.04666085190031, 1.1233270926446]
+
+Y = [-0.046666407409568, 0.069999611114352, 0.166665740748457, 0.253331925937654, 0.326664851866975, 0.389997833351389, 0.433330925945988, 0.469997388910648, 0.486663962985494, 0.493330592615432, 0.489997277800463, 0.469997388910648, 0.433330925945988, 0.38666451853642, 0.323331537052006, 0.249998611122685, 0.156665796303549, 0.053333037039506, -0.063332981484414, -0.189998944453241, -0.333331481496913, -0.486663962985494, -0.65332970373395, -0.789995611147685, -0.646663074104012, -0.516663796320216, -0.403331092611265]
+
+
+
+#############################################################################
+# question 2 : faire afficher les coordonnées du 5ème point de la trajectoire
+#############################################################################
+
+# À COMPLÉTER : faire afficher la 5ème composante de T
+print()
+
+# faire de même pour les listes X et Y
 
 
 #############################################
-## question 3 : construction du tableau des x
+# question 3 : construction de la trajectoire
 #############################################
-debut = 0 # début de l'ensemble des x
-fin = 5 # fin de l'ensemble des x
-pas  = 0.01
-
-# À COMPLÉTER : décommentez la ligne ci-dessous et complétez la fonction pour construire le tableau X
-# X = np.arange()
-
-###############################################
-# question 4 : construction du tableau des I(x)
-###############################################
-# on construit le tableau I image de x par la fonction qui nous intéresse
-
-# À COMPLÉTER
-
-
-
-########################################
-# question 5 : construction de la courbe
-########################################
 
 # À COMPLÉTER : décommentez la ligne ci-dessous et complétez-la
 # plt.plot(,,'b-', label="")
 
 
 #####################################
-# question 6 : noms aux axes et titre
+# question 4 : noms aux axes et titre
 #####################################
 plt.xlabel("axe d'espace")# nom de l'axe selon Ox
 
@@ -48,18 +42,24 @@ plt.xlabel("axe d'espace")# nom de l'axe selon Ox
 
 
 ###############################
-# question 7 : limites des axes
+# question 5 : limites des axes
 ###############################
 
 # À COMPLÉTER : faire que le graphe ait des valeurs limites selon Ox
+
 # À COMPLÉTER : faire que le graphe ait des valeurs limites selon Oy
+
+
 plt.grid()# on affiche une grille
 
 
 ###################################
-# question 8 : légende et affichage
+# question 6 : légende et affichage
 ###################################
 
 # À COMPLÉTER
 # 1 - construire une légende
+
 # 2 - afficher le graphe 
+
+
