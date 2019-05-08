79f999f0eebbfadfda46b5f6ce87d9c461a66022
formationPythonPC-Juin <laurent.astier@ac-limoges.fr>
2019-05-08 17:50:35 +0200

    ajout aide exercice 7

---
+++ exercice7-aide.py
@@ -0,0 +1,65 @@
+#######################################################
+#######################################################
+# Exercice 6
+#######################################################
+#######################################################
+
+import matplotlib.pyplot as plt
+import numpy as np
+from math import radians, cos, sin
+
+################
+# question 1
+################
+...... # entrer m, g et k et leurs valeurs et alpha (en degrés)
+alpha = ... # transformer alpha en radians
+tau = ....# à compléter
+v0 = .....#à compléter : entrer v0 au début
+
+
+################
+# question 2
+################
+.....#à compléter : entrer t au début
+.....# on entre t à la fin
+.....# choisissez-vous un pas de temps
+T = .....#à compléter :  construire l'intervalle de temps d'étude
+
+
+################
+# question 3
+################
+X = v0*cos(alpha)*tau*(1-np.exp(-(T/tau)))
+Y = .....# à compléter
+VX = .....# à compléter
+VY = .....# à compléter
+
+
+################
+# question 4
+################
+# calcul et tracé de la trajectoire
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############
+# question 5
+##############
+plt.plot() # à compléter
